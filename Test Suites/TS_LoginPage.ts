f6101bdda357e3588a20a14eaba64a845f1fc328
Maria <smariaulfah27@gmail.com>
2021-02-23 15:48:01 +0700

    add test suites and report

---
+++ Test Suites/TS_LoginPage.ts
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TS_LoginPage</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>5bf7bd4c-2784-4418-b42c-df489d9716b6</testSuiteGuid>
+   <testCaseLink>
+      <guid>e5db3070-900d-4c2d-8202-ddaecb014baf</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC_LoginP</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
